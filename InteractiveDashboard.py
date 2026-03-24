2d4e41660d12352377f7f019117339e573b514f1
Capdata90 <tecprofiledeleo@gmail.com>
2026-03-23 20:41:21 -0600

    changes

--- InteractiveDashboard.py
+++ InteractiveDashboard.py
@@ -12,7 +12,7 @@ from baseball_field_viz import transform_coords, draw_field, spraychart
 st.set_page_config(page_title="Baseball Visualization - Anthony Volpe", layout="wide")
 
 # Main Title
-st.title("Anthony Volpe batting stats")
+st.title("Anthony Volpe 2025 batting stats")
 st.markdown("---")
 
 # Sidebar for controls
